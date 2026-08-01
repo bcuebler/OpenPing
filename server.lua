67a868c6d39c54e434dca29e4055db69f7a19126
bcuebler <74453403+bcuebler@users.noreply.github.com>
2026-08-01 20:02:13 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,1 +1,12 @@
 
+local PORT = 120
+
+local modem = component.proxy(component.list("modem")
+    modem.open(PORT)
+    while true do
+      local signal = {computer.pullSignal()}
+      if signal[1] == "modem_message" and signal[4] == PORT then
+          local message = signal[6]
+          modem.broadcast(PORT, message)
+      end
+end
