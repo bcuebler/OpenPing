e56625022b14b60789557887d0645af2aba41419
bcuebler <74453403+bcuebler@users.noreply.github.com>
2026-08-03 15:24:48 +0200

    Refactor signal handling in server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,9 @@
-
 local PORT = 120
-
 local modem = component.proxy(component.list("modem")())
-    modem.open(PORT)
-    while true do
-      local signal = {computer.pullSignal()}
-      if signal[1] == "modem_message" and signal[4] == PORT then
-          local message = signal[6]
-          modem.broadcast(PORT, message)
-      end
+modem.open(PORT)
+while true do
+  local signal = {computer.pullSignal()}
+  if signal[1] == "modem_message" and signal[4] == PORT then
+    modem.broadcast(PORT, signal[6])
+  end
 end
